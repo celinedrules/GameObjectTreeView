94ac7e3706de075a9b4c95b3d9968ac4def1d0af
celinedrules <celinedrules@hotmail.com>
2023-12-31 07:07:28 -0700

    Added comments

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -12,29 +12,38 @@ MainWindow::MainWindow(QWidget *parent)
 {
     ui->setupUi(this);
 
+    // Create some GameObjects
     GameObject* object1 = new GameObject("Object1", 10, 12);
     GameObject* object2 = new GameObject("Object2", 0, 0, object1);
     GameObject* object3 = new GameObject("Object 3", 0, 0);
 
+    // Add the GameObjects to the gameObjects list
     gameObjects = QList<GameObject*>();
     gameObjects.append(object1);
     gameObjects.append(object2);
     gameObjects.append(object3);
 
+    // Create the HierarchyTreeView with the gameObjects list
     view = new HierarchyTreeView(gameObjects);
 
+    // Create the Add GameObject button and connect its clicked signal to the onButtonAddClicked slot
     buttonAdd = new QPushButton("Add GameObject");
     QObject::connect(buttonAdd, &QPushButton::clicked, this, &MainWindow::onButtonAddClicked);
 
+    // Create the Show GameObject Info button and connect its clicked signal to the onButtonInfoClicked slot
     buttonInfo = new QPushButton("Show GameObject Info");
     QObject::connect(buttonInfo, &QPushButton::clicked, this, &MainWindow::onButtonInfoClicked);
 
+    // Create a QVBoxLayout and add the HierarchyTreeView and buttons to it
     QVBoxLayout *layout = new QVBoxLayout();
     layout->addWidget(view);
     layout->addWidget(buttonAdd);
     layout->addWidget(buttonInfo);
 
+    // Set the layout of the central widget to the QVBoxLayout
     ui->centralwidget->setLayout(layout);
+
+    // Update the HierarchyTreeView
     view->updateTreeView();
 }
 
@@ -45,13 +54,16 @@ MainWindow::~MainWindow()
 
 void MainWindow::onButtonAddClicked()
 {
+    // Add an empty GameObject to the HierarchyTreeView
     view->addEmptyGameObject();
 }
 
 void MainWindow::onButtonInfoClicked()
 {
+    // Get the currently selected GameObject in the HierarchyTreeView
     GameObject* gameObject = view->getCurrentGameObject();
 
+    // If a GameObject is selected, show a message box with its info
     if (gameObject) {
         QMessageBox::information(nullptr, "GameObject Info",
             QString("Name: %1\n x: %2\ny: %3").arg(gameObject->name()).arg(gameObject->x()).arg(gameObject->y()));
@@ -60,16 +72,19 @@ void MainWindow::onButtonInfoClicked()
 
 bool MainWindow::eventFilter(QObject *obj, QEvent *event)
 {
+    // Check if the event was a mouse button press on the viewport of the HierarchyTreeView
     if (obj == view->viewport() && event->type() == QEvent::MouseButtonPress) {
+        // If so, cast the event to a QMouseEvent
         QMouseEvent* mouseEvent = static_cast<QMouseEvent*>(event);
+        // Get the index at the position of the mouse event
         QModelIndex index = view->indexAt(mouseEvent->pos());
+
+        // If the index is not valid (i.e., the click was not on an item), clear the selection
         if (!index.isValid()) {
-            // The click was not on an item, clear the selection
             view->clearSelection();
         }
     }
+
     // Pass the event on to the parent class
     return QMainWindow::eventFilter(obj, event);
 }
-
-
--- mainwindow.h
+++ mainwindow.h
@@ -14,26 +14,70 @@ QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
 
+/**
+ * @class MainWindow
+ * @brief Main window of the application
+ *
+ * This class represents the main window of the application. It inherits from QMainWindow and contains various UI elements and game objects
+ */
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
 
 public:
+    /**
+     * @brief Constructs a MainWindow with the given parent
+     *
+     * @param parent The parent widget of the MainWindow
+     */
     MainWindow(QWidget *parent = nullptr);
+
+    /**
+     * @brief Destructor for MainWindow
+     */
     ~MainWindow();
+
+    /**
+     * @brief Finds an item in the model corresponding to a GameObject
+     *
+     * @param gameObject The GameObject to find
+     * @param parentItem The parent item to start the search from
+     * @return The found item, or nullptr if no item was found
+     */
     QStandardItem *findItemInModel(const GameObject *gameObject, QStandardItem *parentItem) const;
 public slots:
+    /**
+     * @brief Slot to handle the Add button being clicked
+     */
     void onButtonAddClicked();
+
+    /**
+     * @brief Slot to handle the Info button being clicked
+     */
     void onButtonInfoClicked();
 
 private:
-    Ui::MainWindow *ui;
-    QList<GameObject*> gameObjects;
-    HierarchyTreeView *view;
-    QPushButton *buttonAdd;
-    QPushButton *buttonInfo;
-    QHash<GameObject*, QStandardItem*> map;
+    /**
+     * @brief Filters events for the MainWindow
+     *
+     * @param obj The object that the event was sent to
+     * @param event The event that was sent
+     * @return True if the event was filtered (i.e., it should be ignored), false otherwise
+     */
     bool eventFilter(QObject* obj, QEvent* event);
+
+    // The UI for the MainWindow
+    Ui::MainWindow *ui;
+    // The list of game objects
+    QList<GameObject*> gameObjects;
+    // The hierarchy tree view
+    HierarchyTreeView *view;
+    // // The Add button
+    QPushButton *buttonAdd;
+    // The Info button
+    QPushButton *buttonInfo;
+    // A map from GameObjects to items in the model
+    QHash<GameObject*, QStandardItem*> map;
 };
 
 #endif // MAINWINDOW_H
